9df43c96b787eeadf2261f4203c1ae29b4acecbd
Karl San Gabriel <karl.sangabriel@gmail.com>
2019-01-01 14:06:22 +0800

    Initial files generated from Laravel via composer

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::post('/register', function(Request $request) {
+    return 'Test';
+});
+
+Route::post('/deregister', function(Request $request) {
+    return 'Test';
+});
+
+
+Route::get('/services', function(Request $request) {
+    return 'Test';
+});
+
+
+
+
